d90a35cca0573ba6976f1da6abea62c88bce98fd
Kazu Yamamoto <kazu@iij.ad.jp>
2011-06-14 16:06:30 +0900

    Revert "cosmetic change."
    This reverts commit 50f9fca0a5a821f5c7a63702911d2cd83f619bf2.

--- Network/Wai/Application/Classic/File.hs
+++ Network/Wai/Application/Classic/File.hs
@@ -10,6 +10,7 @@ import qualified Data.ByteString as BS hiding (unpack, pack)
 import qualified Data.ByteString.Char8 as BS (pack)
 import qualified Data.ByteString.Lazy.Char8 as BL (length)
 import Network.HTTP.Types
+import Network.Socket (Socket)
 import Network.Wai
 import Network.Wai.Application.Classic.Field
 import Network.Wai.Application.Classic.FileInfo
