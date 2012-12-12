e50a25e5bc15ae49eb47dcaf41b8744fda86ffe8
Kazu Yamamoto <kazu@iij.ad.jp>
2012-12-12 16:53:18 +0900

    Using default values from lib.

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,20 +3,14 @@
 module Main where
 
 import ClassicSpec
-import Control.Applicative
 import Control.Concurrent
-import Control.Exception
 import Control.Monad
 import qualified Data.ByteString as BS
-import Network.HTTP.Date
 import Network.Wai
 import Network.Wai.Application.Classic hiding ((</>))
 import Network.Wai.Handler.Warp
-import Network.Wai.Logger
 import System.Directory
 import System.FilePath
-import System.Log.FastLogger.Date
-import System.Posix
 import Test.Hspec
 
 main :: IO ()
@@ -36,7 +30,7 @@ testApp dir req
     | otherwise = fileApp appSpec defaultFileAppSpec fileRoute req
   where
     cgi = "/cgi-bin/" `BS.isPrefixOf` rawPathInfo req
-    appSpec = defaultAppSpec { softwareName = "ClassicTester" }
+    appSpec = defaultClassicAppSpec { softwareName = "ClassicTester" }
     cgiRoute = CgiRoute {
         cgiSrc = "/cgi-bin/"
       , cgiDst = fromString (dir </> "test/cgi-bin/")
@@ -45,49 +39,3 @@ testApp dir req
         fileSrc = "/"
       , fileDst = fromString (dir </> "test/html/")
       }
-
-defaultAppSpec :: ClassicAppSpec
-defaultAppSpec = ClassicAppSpec {
-    softwareName = "Classic"
-  , logger = defaultLogger
-  , dater = defaultDater
-  , statusFileDir = "/usr/local/share/html/status"
-  }
-
-defaultLogger :: ApacheLogger
-defaultLogger _ _ _ = return ()
-
-defaultDater :: IO ZonedDate
-defaultDater = formatHTTPDate . epochTimeToHTTPDate <$> epochTime
-
-defaultCgiAppSpec :: CgiAppSpec
-defaultCgiAppSpec = CgiAppSpec {
-    indexCgi = "index.cgi"
-  }
-
-defaultFileAppSpec :: FileAppSpec
-defaultFileAppSpec = FileAppSpec {
-    indexFile = "index.html"
-  , isHTML = defaultIsHTml
-  , getFileInfo = defaultGetFileInfo
-  }
-
-defaultIsHTml :: Path -> Bool
-defaultIsHTml file = ".html" `isSuffixOf` file || ".htm" `isSuffixOf` file
-
-defaultGetFileInfo :: Path -> IO FileInfo
-defaultGetFileInfo path = do
-    fs <- getFileStatus sfile
-    if not (isDirectory fs) then
-        return FileInfo {
-            fileInfoName = path
-          , fileInfoSize = size fs
-          , fileInfoTime = time fs
-          , fileInfoDate = formatHTTPDate (time fs)
-          }
-      else
-        throwIO $ userError "does not exist"
-  where
-    sfile = pathString path
-    size = fromIntegral . fileSize
-    time = epochTimeToHTTPDate . modificationTime
