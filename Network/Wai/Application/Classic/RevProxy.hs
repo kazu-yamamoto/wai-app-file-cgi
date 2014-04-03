a4e657bcd12c62de481221d5ad9f99e6c59a9dc5
Kazu Yamamoto <kazu@iij.ad.jp>
2014-04-03 16:19:37 +0900

    catching up conduit 1.1.

--- Network/Wai/Application/Classic/RevProxy.hs
+++ Network/Wai/Application/Classic/RevProxy.hs
@@ -12,7 +12,7 @@ import qualified Data.ByteString.Char8 as BS hiding (uncons)
 import Data.Conduit
 import Data.Default.Class
 import qualified Network.HTTP.Client as H
-import qualified Network.HTTP.Client.Conduit as H
+import qualified Network.HTTP.Conduit as H
 import Network.HTTP.Types
 import Network.Wai
 import Network.Wai.Application.Classic.Conduit
